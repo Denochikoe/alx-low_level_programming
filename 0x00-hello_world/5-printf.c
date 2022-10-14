5fa28a6678684460228b2c4375ef757b5b7bb024
Denochikoe <denochikoe@gmail.com>
2022-10-14 09:34:04 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,49 +1,14 @@
-lude <stdio.h>
-
-
+#include <stdio.h>
 
 /**
+ *main - Entry point
  *
- * * main - Prints "with proper grammar, but the ouptu is a piece of art,"
+ * Description: 'the program outputs a string'
  *
- * *
- *
- * * Return: Always 0 (Success)
- *
- * */
-
-
-
+ *Return: Always 0 (Success)
+ */
 int main(void)
-
 {
-
-		printf("with proper grammar, but the outcome is a piece of art,\n");
-
-			return (0);
-
-}#include <stdio.h>
-
-
-
-/**
- *
- * * main - Prints "with proper grammar, but the ouptu is a piece of art,"
- *
- * *
- *
- * * Return: Always 0 (Success)
- *
- * */
-
-
-
-int main(void)
-
-{
-
-		printf("with proper grammar, but the outcome is a piece of art,\n");
-
-			return (0);
-
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
 }
