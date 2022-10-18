bcb4a9fba4d561c4ea8245f70b350af9c24cbbce
Denochikoe <denochikoe@gmail.com>
2022-10-17 17:28:37 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ *main-Entry point
+ *
+ * Description: 'the program outputs {a..z} in one line!'
+ *
+ * Return: Always (0) success
+ *
+ */
+int main(void)
+{
+	char i = 'a';
+	while(i <= 'z')
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
+
