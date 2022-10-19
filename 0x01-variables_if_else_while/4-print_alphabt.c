8fabe81f178d4f9530f84501c86bae03b50d8ef2
Denochikoe <denochikoe@gmail.com>
2022-10-20 02:19:09 +0300

    4-print_alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main-Entry point
+ *
+ * Description:'the program a-z except for q and e'
+ *
+ * Return (0) success
+ *
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+	if (i != 'q' && i != 'e')
+	putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
