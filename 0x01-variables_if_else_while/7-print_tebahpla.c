9b49278c7da2843b77b05059330091a16766752d
Denochikoe <denochikoe@gmail.com>
2022-10-20 09:41:09 +0300

    7-print_tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main-Entry point
+ *
+ * Description:'the program reverses 9-0'
+ *
+ * return (0) success
+ *
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 'z'; i >= 'a'; i--)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
