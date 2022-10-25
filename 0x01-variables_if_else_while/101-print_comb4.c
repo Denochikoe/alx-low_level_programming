c2207bbeea48a3941b0af1c0b97e7b4a4e77c734
Denochikoe <denochikoe@gmail.com>
2022-10-25 01:49:58 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -9,24 +9,26 @@
  */
 int main(void)
 {
-	int i, j, k;
+	int i, j, k, l;
 
-	i = 0;
-	while (i < 790)
+	for (i = 0; i < 1000; i++)
 	{
-		j = i % 10;
-		k = i / 10;
-		if (k < j)
+		j = i / 100;
+		k = (i / 10) % 10;
+		l = i % 10;
+
+		if (j < k && k < l)
 		{
-			putchar(k + '0');
 			putchar(j + '0');
-			if (i < 89)
+			putchar(k + '0');
+			putchar(l + '0');
+
+			if (i < 700)
 			{
 				putchar(44);
 				putchar(32);
 			}
 		}
-		i++;
 	}
 	putchar('\n');
 	return (0);
