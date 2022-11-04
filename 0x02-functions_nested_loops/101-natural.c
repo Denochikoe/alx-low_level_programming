68187ce1c5a876b6b30f16d7c298f660d6afcd71
Denochikoe <denochikoe@gmail.com>
2022-11-04 13:50:04 -0700

    101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main  - Entry point
+ * Description- 'the program print multiples of  3 or 5'
+ * Return: Always (0) success
+ */
+int main(void)
+{
+	int i;
+	int mul;
+
+	for (i = 0; i <1024; i++)
+	{
+		if ( i % 3 == 0 || i % 5 == 0)
+		{
+			mul += i;
+		}
+	}
+	printf("%d\n", mul);
+	return (0);
+}
