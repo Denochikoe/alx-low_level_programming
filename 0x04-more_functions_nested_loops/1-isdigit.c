a8c4246985b49cd783940ef9c8db20a7ebc183fa
Denochikoe <denochikoe@gmail.com>
2022-11-09 09:50:06 -0800

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isdigit- checks for digit 0-9
+ * c@: variable declaration
+ * Return (1) or  (0)
+ */
+int _isdigit(int c)
+{
+	for (c >= 1; c <= 9; c++)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
