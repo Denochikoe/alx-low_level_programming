e5993a414490a47b00c29c58e5756871ce731f29
Denochikoe <denochikoe@gmail.com>
2022-11-18 18:01:14 +0300

    0-reset_to_98.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(void)
+{
+	int i;
+	int j;
+	int val;
+
+	val = i * j;
+	putchar(val + '0');
+	putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(void)
+{
+	int a = 9, b = 0;
+	int val;
+
+	while ( b > 9)
+	{
+		putchar(val + '0');
+		putchar(',');
+	}
+	putchar('\n');
+}
