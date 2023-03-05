9dc124acb16504855e0a1607cba6b25956d993fc
Denochikoe <denochikoe@gmail.com>
2023-03-05 01:11:11 -0800

    1-print_rev_recursion

---
+++ 0x09-static_libraries/0-main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/*
+ * main - returns 0 on success
+ * Return: on succes (0) 
+ */
+int main()
+{
+	return (0);
+}
---
+++ 0x09-static_libraries/1-isdigit.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _isdigit- checks for digit 0-9
+ * c@: variable declaration
+ * Return (1) or  (0)
+ */
+int _isdigit(int c)
+{
+	return (c >= '0' &&  c <= '9');
+}
