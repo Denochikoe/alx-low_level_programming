c17f4effa8be947a6981199034aed66435b772dd
Denochikoe <denochikoe@gmail.com>
2023-04-22 17:42:56 +0300

    4-new_dog.c

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,44 @@
+#include "dog.h"
+#include <stdlib.h>
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	int i, j, k;
+
+	dog_t *dog;
+	if (name == NULL || owner == NULL)
+	{
+		return(NULL);
+	}
+	dog = malloc(sizeof(dog_t));
+	if (dog == NULL)
+		return(NULL);
+	for (i = 0; name[i]; i++)
+		;
+	i++;
+	dog->name = malloc(sizeof(char) * i);
+	if (dog->name == NULL)
+	{
+		free(dog);
+		return(NULL);
+	}
+	for (k = 0; k < i; k++)
+	{
+		dog->name[k] = name[k];
+	}
+	dog->age = age;
+	for (j = 0; owner[j]; j++)
+		;
+	j++;
+	dog->owner = malloc(sizeof(char) * j);
+	if (dog->owner == NULL)
+	{
+		free(dog->owner);
+		free(dog);
+		return(NULL);
+	}
+	for (k = 0; k < j; k++)
+	{
+		dog->owner[k] = owner[k];
+	}
+		return(dog);
+}
