604ad9c94257294c6fbbde514dfb3c6a43476261
Denochikoe <denochikoe@gmail.com>
2023-05-14 11:34:15 +0300

    3-print_all.c

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -4,4 +4,10 @@
 void print_numbers(const char *separator, const unsigned int n, ...);
 int sum_them_all(const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+typedef struct print
+{
+	char *t;
+	void (*f)(va_list);
+}print_t;
+void print_all(const char * const format, ...);
 #endif
