2e88ccee06d72964ee64e49a1b071a72a56ecf9b
Denochikoe <denochikoe@gmail.com>
2023-10-28 21:30:13 +0300

    fix

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -3,14 +3,14 @@
  *_pow_recursion - prints x to the power of y.
  * @x:param 1
  * @y:param 2
- * return: (0) success.
+ * Return: (0) success.
  */
 int _pow_recursion(int x, int y)
 {
-	if(y < 0)
-		return(-1);
-	else if(y == 0)
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
 		return (1);
 	else
-		return(x * _pow_recursion(x, y - 1));
+		return (x * _pow_recursion(x, y - 1));
 }
