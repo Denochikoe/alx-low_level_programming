30dec006289db19abe5a669c1b00ed66fd1f300e
Denochikoe <denochikoe@gmail.com>
2023-10-28 20:01:57 +0300

    Fix

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -2,13 +2,13 @@
 /**
  * factorial- calculates the factorial of an integer.
  * @n: param 1
- * return:(0) success
+ * Return:(0) success
  */
 int factorial(int n)
 {
-	if(n < 0)
+	if (n < 0)
 		return (-1);
-	else if(n == 1)
+	else if (n == 1)
 		return (1);
 	else
 		return (n * factorial(n - 1));
