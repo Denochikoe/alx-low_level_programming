636fca857a72e5af87cb605aafee71e8e442bd4c
Denochikoe <denochikoe@gmail.com>
2023-10-29 21:19:56 +0300

    fix

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,29 +1,33 @@
 #include "main.h"
-int squareroot(int n, int i);
 /**
- * _sqrt_recursion- find squareroot
- * @n:square
- * return:squareroot
+ * find_sqrt - find the squareroot y checking if a number is
+ * applicable
+ * @n: number to get squareroot of
+ * @g: quessing parameter
+ * Return: squareroot or -1
+ */
+int find_sqrt(int n, int g)
+{
+if (g * g == n)
+{
+return (g);
+}
+else if (g * g > n)
+{
+return (-1);
+}
+return (find_sqrt(n, g + 1));
+}
+/**
+ * _sqrt_recursion - finds the squareroot of a number
+ * @n: number to find square root
+ * Return: square root
  */
 int _sqrt_recursion(int n)
 {
-	if(n < 0)
-		return(-1);
-	else
-		return(squareroot(n, (n + 1) / 2));
-}
-/**
- * squareroot- finds perfect squares
- * @n:param 1
- * @i:param 2
- * return: square
- */
-int squareroot(int n, int i)
+if (n == 0)
 {
-	if(n < 1)
-	return(-1);
-	else if(i * i == n)
-	return(i);
-	else
-	return(squareroot(n, i - 1));
+return (0);
+}
+return (find_sqrt(n, 1));
 }
