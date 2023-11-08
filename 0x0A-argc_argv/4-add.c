ca1ed22fe5b75826277e4cbd63e4396034b2baf8
Denochikoe <denochikoe@gmail.com>
2023-11-08 03:28:00 +0300

    4-add

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -10,21 +10,21 @@
 int main(int argc, char *argv[])
 {
 	int i;
-	int j;
 	int sum = 0;
 
-	for (i = 0; i < argc; i++)
+	if(argc < 0)
 	{
-		for (j = 0; j < argv[i][j]; j++)
-		{
-			if(isdigit(argv[i][j]) == 0)
-			{
-				printf("%s\n", "Error");
-				return(1);
-			}
-			sum += atoi(argv[i]) + atoi(argv[j]);
-		}
-		printf("%i\n", sum);
-		return(0);
+		return (0);
 	}
+	for(i = 1; i < argc; i++)
+	{
+		if(!isdigit(*argv[i]))
+		{
+			printf("Error\n");
+			return (1);
+		}
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+	return (0);
 }
