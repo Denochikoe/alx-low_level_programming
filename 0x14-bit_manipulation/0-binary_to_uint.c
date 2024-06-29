5d8152114c537d9392b210eabc19bb007bbad821
Denochikoe <denochikoe@gmail.com>
2024-06-30 00:00:25 +0300

    fix

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,8 +8,8 @@ unsigned int binary_to_uint(const char *b)
 {
 	int i;
 	unsigned int num;
-	num = 0;
 
+	num = 0;
 	if (!b)
 		return (0);
 	for (i = 0; b[i] != '\0'; i++)
@@ -21,9 +21,7 @@ unsigned int binary_to_uint(const char *b)
 	{
 		num <<= 1;
 		if (b[i] == '1')
-		{
 			num += 1;
-		}	
 	}
 	return (num);
 }
