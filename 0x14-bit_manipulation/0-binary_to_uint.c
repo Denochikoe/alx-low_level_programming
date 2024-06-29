cd92e111420eab380f533bac5df169e676b216f1
Denochikoe <denochikoe@gmail.com>
2024-06-29 23:55:12 +0300

    fix

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,6 +9,7 @@ unsigned int binary_to_uint(const char *b)
 	int i;
 	unsigned int num;
 	num = 0;
+
 	if (!b)
 		return (0);
 	for (i = 0; b[i] != '\0'; i++)
@@ -20,8 +21,9 @@ unsigned int binary_to_uint(const char *b)
 	{
 		num <<= 1;
 		if (b[i] == '1')
+		{
 			num += 1;
-			
+		}	
 	}
 	return (num);
 }
