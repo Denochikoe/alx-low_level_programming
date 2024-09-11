dc647b470428db52a4a452e4e53cd666aa908cf7
Denochikoe <denochikoe@gmail.com>
2024-09-11 20:06:22 +0300

    1-dlistint_len.c

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -9,12 +9,10 @@
 size_t dlistint_len(const dlistint_t *h)
 {
 	size_t count = 0;
-	
 	while (h != NULL)
-    {
-        count++;
-        h = h->next;  /* Move to the next node */
-    }
-
-    return count;  /* Return the total number of elements */
+       	{
+		count++;
+		h = h->next;
+	}
+	return count;
 }
